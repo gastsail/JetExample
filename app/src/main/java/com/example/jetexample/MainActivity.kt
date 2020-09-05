8afc88a2a5226835734ad4efa3843233ce4f6d10
Gaston Saillen <gsaillen95@gmail.com>
2020-09-05 14:52:28 -0300

    Lista con compose

--- app/src/main/java/com/example/jetexample/MainActivity.kt
+++ app/src/main/java/com/example/jetexample/MainActivity.kt
@@ -8,9 +8,11 @@ import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumnFor
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawShadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.imageResource
@@ -19,55 +21,58 @@ import androidx.ui.tooling.preview.Preview
 
 import com.example.jetexample.ui.typography
 
-val recipeList = listOf(Recipe(R.drawable.header,"Test", listOf("Azucar","Tomate","lasagna")),
-        Recipe(R.drawable.header,"Test", listOf("Azucar","Tomate","lasagna")),
-        Recipe(R.drawable.header,"Test", listOf("Azucar","Tomate","lasagna")))
+data class Recipe(
+        @DrawableRes val imageResource: Int,
+        val title: String,
+        val ingredients: List<String>
+)
 
+val recipeList = listOf(Recipe(R.drawable.header,"Arrozmate", listOf("Arroz","Tomate","Crema")),
+        Recipe(R.drawable.header,"Calabaza", listOf("Queso","Azucar","Agua")),
+        Recipe(R.drawable.header,"Torta", listOf("Merengue","Chocolate","Vainilla")),
+        Recipe(R.drawable.header,"Torta2", listOf("Merengue2","Chocolate2","Vainilla2")))
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RecipeList(recipeList)
+            RecipeList(recipeList = recipeList)
         }
     }
 }
 
 @Composable
-fun RecipeCard(recipe: Recipe) {
+fun RecipeCard(recipe:Recipe){
     val image = imageResource(R.drawable.header)
-    Column(modifier = Modifier.padding(16.dp)) {
-        val imageModifier = Modifier
-                .preferredHeight(150.dp)
-                .fillMaxWidth()
-                .clip(shape = RoundedCornerShape(8.dp))
-
-        Image(image,modifier= imageModifier, contentScale = ContentScale.Crop)
-        Spacer(Modifier.preferredHeight(16.dp))
-        Text(recipe.title, style = typography.h6)
-        for(ingredient in recipe.ingredients){
-            Text(ingredient,style = typography.body2)
+    Surface(shape = RoundedCornerShape(8.dp),elevation = 8.dp,modifier = Modifier.padding(8.dp)) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            val imageModifier = Modifier.preferredHeight(150.dp).fillMaxWidth().clip(shape = RoundedCornerShape(8.dp))
+            Image(asset = image,modifier = imageModifier,contentScale = ContentScale.Crop)
+            Spacer(modifier = Modifier.preferredHeight(16.dp))
+            Text(text = recipe.title,style = typography.h6)
+            for(ingredient in recipe.ingredients){
+                Text(text = ingredient,style = typography.body2)
+            }
         }
     }
 }
 
 @Composable
 fun RecipeList(recipeList:List<Recipe>){
-    LazyColumnFor(recipeList) { item ->
+    LazyColumnFor(items = recipeList) { item ->
         RecipeCard(recipe = item)
     }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 fun RecipePreview(){
     RecipeCard(recipeList[0])
 }
 
-data class Recipe(
-        @DrawableRes val imageResource: Int,
-        val title: String,
-        val ingredients: List<String>
-)
+
+
+
+
 
 
