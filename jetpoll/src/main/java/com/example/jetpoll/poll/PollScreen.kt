507ce3b802cea53ed2e2f9cd7e6fd3edcee0e5e7
Gaston Saillen <gsaillen95@gmail.com>
2020-09-18 21:21:04 -0300

    UI changes to the card WIP

--- jetpoll/src/main/java/com/example/jetpoll/poll/PollScreen.kt
+++ jetpoll/src/main/java/com/example/jetpoll/poll/PollScreen.kt
@@ -5,10 +5,11 @@ import androidx.compose.foundation.Box
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.RowScope.align
 import androidx.compose.foundation.lazy.LazyRowFor
-import androidx.compose.material.Button
-import androidx.compose.material.Card
-import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
@@ -19,22 +20,32 @@ import com.example.jetpoll.presentation.PollViewModel
 import com.example.jetpoll.vo.*
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.ContextAmbient
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
 import com.example.jetpoll.data.model.Option
 import com.example.jetpoll.utils.showMessage
 
 @Composable
 private fun PollComponent(poll: Poll, onOptionClick: (Option) -> Unit, onViewPollClick: (Poll) -> Unit) {
-    Card(modifier = Modifier.fillMaxWidth().padding(8.dp)) {
-        Column {
-            Text(text = poll.question)
+    Card(modifier = Modifier.width(300.dp).padding(16.dp),elevation = 8.dp,shape = RoundedCornerShape(8.dp)) {
+        Column(modifier = Modifier.padding(8.dp)) {
+            Text(text = poll.question,modifier = Modifier.fillMaxWidth(),style = TextStyle(fontSize = 24.sp),fontWeight = FontWeight.Bold)
             Spacer(modifier = Modifier.padding(top = 16.dp))
             for (option in poll.options) {
-                Text(text = option.name, modifier = Modifier.clickable(onClick = { onOptionClick(option) }))
+                OutlinedButton(modifier = Modifier.fillMaxWidth().padding(top = 4.dp,end = 8.dp,start = 8.dp),onClick = {onOptionClick(option)}) {
+                    Text(text = option.name)
+                }
             }
+
+            Spacer(modifier = Modifier.padding(top = 16.dp))
+
             Button(onClick = {
                 onViewPollClick(poll)
-            }) {
+            },modifier = Modifier.width(200.dp).align(Alignment.CenterHorizontally).clip(CircleShape)) {
                 Text(text = "View poll")
             }
         }
@@ -44,14 +55,14 @@ private fun PollComponent(poll: Poll, onOptionClick: (Option) -> Unit, onViewPol
 @Composable
 private fun PollList(pollList: List<Poll>) {
     val context = ContextAmbient.current
-    LazyRowFor(items = pollList) { poll ->
-        PollComponent(poll = poll,
-                onOptionClick = { option ->
-                    showMessage(context,"Voted for $option")
-                }, onViewPollClick = {
-                    showMessage(context,"Clicked to view poll results")
-        })
-    }
+        LazyRowFor(items = pollList) { poll ->
+            PollComponent(poll = poll,
+                    onOptionClick = { option ->
+                        showMessage(context,"Voted for $option")
+                    }, onViewPollClick = {
+                showMessage(context,"Clicked to view poll results")
+            })
+        }
 }
 
 
